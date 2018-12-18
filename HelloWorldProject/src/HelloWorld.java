8bac1476f38ec4bb5c2c555a8c8d0a5872aef011
laure <laure@10.100.129.111>
2018-12-18 18:06:06 +0100

    primitiv variablen, short, int, long

--- HelloWorldProject/src/HelloWorld.java
+++ HelloWorldProject/src/HelloWorld.java
@@ -3,7 +3,12 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Hallo Klasse");
+		System.out.println("Hallo Lenzi");
+// das ist ein Kommentar, kein Code der ausgeführt wird
+// das ist ein einzeiliger Kommentar
+		/*
+		 * Mehrzeiligen Kommentar schreiben
+		 */
 
 	}
 
