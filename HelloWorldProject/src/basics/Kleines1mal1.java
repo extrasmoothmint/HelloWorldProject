373bfba80ecc03469c2c3f82558ceb6a523fa58f
laure <laure@10.100.129.153>
2019-01-15 18:04:41 +0100

    Kleines 1 mal 1

---
+++ HelloWorldProject/src/basics/Kleines1mal1.java
@@ -0,0 +1,12 @@
+package basics;
+
+public class Kleines1mal1 {
+	public static void main(String[] args) {
+	{for(int x=1; x<=10; x++) {
+	
+		for(int y=1; y<=10; y++) 
+			System.out.print("\t" + (y*x)); System.out.println();
+	}
+}
+}
+}
